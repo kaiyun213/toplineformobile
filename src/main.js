8ac2a1bd9be99d6f2aec32a60da917da933cf4fb
kaiyun213 <996741645@qq.com>
2019-09-14 21:25:27 +0800

    登录页功能完成(没有模块封装)

--- src/main.js
+++ src/main.js
@@ -9,6 +9,20 @@ import 'vant/lib/index.css';
 //使用vant
 Vue.use(Vant);
 
+//导入插件vee-validate
+import VeeValidate,{Validator} from 'vee-validate'
+//使用插件vee-validate,并禁止默认事件的触发
+Vue.use(VeeValidate,{
+ events:''
+})
+//导入本地语言包
+import zh_CN from 'vee-validate/dist/locale/zh_CN.js'
+//使用本地语言包
+Validator.localize('zh_CN', zh_CN);
+
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
