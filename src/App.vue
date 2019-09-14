ceefa06d56fc813239e788ac021f2cf0cda979d6
kaiyun213 <996741645@qq.com>
2019-09-14 19:44:56 +0800

    完成登录页面的布局

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <h2>这是App.vue页面</h2>
+  <div class="app">
     <router-view></router-view>
   </div>
 </template>
@@ -12,5 +11,7 @@ export default {
 </script>
 
 <style>
-
+.app {
+  height: 100%;
+}
 </style>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,15 +1,42 @@
 <template>
-  <div>
-      <h2>这里是login页面</h2>
+  <div class="login">
+    <van-nav-bar title="登录" />
+    <van-cell-group>
+      <van-field v-model="mobile" required clearable label="手机号码" placeholder="请输入手机号码" />
+      <van-field v-model="code" label="验证码" placeholder="请收入验证码" required />
+    </van-cell-group>
+    <div class="login-btn">
+      <van-button type="info" size="large">大号按钮</van-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      mobile: '',
+      code: '246810',
+    }
+  },
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.login {
+  height: 100%;
+  background-color: #f5f7f9;
+  .van-nav-bar {
+    background-color: #3296fa;
+    .van-nav-bar__title {
+      color: #fff;
+    }
+  }
+  .login-btn {
+    margin:  30px 10px;
+    .van-button{
+    border-radius: 8px;
+    }
+  }
+}
 </style>
