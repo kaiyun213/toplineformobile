51ceb58b8c27485d1305b2e1cfdeff88cc0d2f34
kaiyun213 <996741645@qq.com>
2019-09-15 08:30:39 +0800

    使用vuex统一管理数据

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,18 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
+//引入auther数据
+import * as auther from '@/utils/author'
 export default new Vuex.Store({
   state: {
-
+    //保存你数据到vuex中
+    use: auther.getAuthor()
   },
   mutations: {
-
+    //设置use
+    setUse(state, use) {
+      auther.setAuthor(use)
+    }
   },
   actions: {
 
