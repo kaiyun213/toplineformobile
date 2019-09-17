c517f78a6adc0627b081ec7ba8ad0fcc9372557a
kaiyun213 <996741645@qq.com>
2019-09-18 00:40:04 +0800

    添加频道和删除频道功能完成

--- src/api/channel.js
+++ src/api/channel.js
@@ -5,23 +5,34 @@
 import request from '@/utils/request'
 
 //发送请求
-function channelRequest(){
+function channelRequest() {
     return request({
-     url:'/app/v1_0/user/channels',
-     method:'GET',
-     //这里需要设置请求头,在axios中统一设置请求头
+        url: '/app/v1_0/user/channels',
+        method: 'GET',
+        //这里需要设置请求头,在axios中统一设置请求头
     })
 }
 
 //获取所有的频道
-function getAllChannel(){
+function getAllChannel() {
     return request({
-        url:'/app/v1_0/channels',
-        method:'GET',
+        url: '/app/v1_0/channels',
+        method: 'GET',
+    })
+}
+
+//设置用户频道
+function setUserChannel({ channels }) {
+    return request({
+        url: '/app/v1_0/user/channels',
+        method: 'PUT',
+        data: {
+            channels,
+        }
     })
 }
 
 
 //暴露函数
 
-export {channelRequest, getAllChannel}
+export { channelRequest, getAllChannel, setUserChannel }
--- src/views/Home/channel/index.vue
+++ src/views/Home/channel/index.vue
@@ -18,11 +18,11 @@
             <button v-else @click="completeChannel">完成</button>
           </div>
         </div>
-        <van-grid :gutter="10" clickable>
+        <van-grid :gutter="2" clickable>
           <van-grid-item v-for="(item,index) in channelArr" :key="item.id">
             <div slot="text" class="text-channel" @click="transferActive(index)">
               <div :class="{active:index===active}">{{item.name}}</div>
-              <van-icon class="close-icon" v-show="isShow" name="close" />
+              <van-icon class="close-icon" v-show="isShow" name="close" @click="delChannel(index)" />
             </div>
           </van-grid-item>
         </van-grid>
@@ -36,7 +36,7 @@
         </div>
         <van-grid :gutter="2" clickable>
           <van-grid-item v-for="item in otherChannelList" :key="item.id">
-            <div slot="text">
+            <div slot="text" @click="addChannel(item)">
               <!-- <van-icon name="plus" /> -->
               {{item.name}}
             </div>
@@ -49,7 +49,7 @@
 
 <script>
 //导入获取全部频道年数据的方法
-import {getAllChannel} from '@/api/channel.js'
+import { getAllChannel, setUserChannel } from '@/api/channel.js'
 export default {
   props: ['value', 'channelArr', 'active'],
   data() {
@@ -57,7 +57,7 @@ export default {
       //关闭图标是否显示
       isShow: false,
       //所有的频道列表
-      allChannelList:[],
+      allChannelList: [],
     }
   },
   methods: {
@@ -78,7 +78,7 @@ export default {
     async getAllChannelList() {
       let res = await getAllChannel()
       // console.log(res)
-      this.allChannelList=res.channels
+      this.allChannelList = res.channels
       //添加属性
       this.allChannelList.forEach(item => {
         //添加文章列表
@@ -92,18 +92,64 @@ export default {
         //添加上一页数据的事件戳  pre_timestamp
         this.$set(item, 'pre_timestamp', 0)
       })
+    },
+
+    //添加频道
+    async addChannel(item) {
+      //将当前的频道添加到我的频道列表中
+      this.channelArr.push(item)
+      //判断是否登录
+      let use = this.$store.state.use
+      if (use && use.token) {
+        //有登录的情况
+        //先处理参数
+        let channels = this.channelArr.slice(1).map((item, index) => {
+          return {
+            id: item.id,
+            seq: index + 2
+          }
+        })
+        await setUserChannel({
+          channels, channels
+        })
+      } else {
+        //未登录情况,将数据保存到本地
+        window.localStorage.setItem('channnel', JSON.stringify(this.channelArr))
+      }
+    },
+    //删除频道
+    async delChannel(index) {
+      this.channelArr.splice(index, 1)
+      //判断是否登录
+      let use = this.$store.state.use
+      if (use && use.token) {
+        //有登录的情况
+        //先处理参数
+        let channels = this.channelArr.slice(1).map((item, index) => {
+          return {
+            id: item.id,
+            seq: index + 2
+          }
+        })
+        await setUserChannel({
+          channels, channels
+        })
+      } else {
+        //未登录情况,将数据保存到本地
+        window.localStorage.setItem('channnel', JSON.stringify(this.channelArr))
+      }
     }
   },
   computed: {
     //通过计算属性将getAllChannelList里面的数据取出channelArr里面的数据之后,显示在推荐频道上
-    otherChannelList(){
+    otherChannelList() {
       //取出channelArr里面的id
-     let ids= this.channelArr.map(item=>{
+      let ids = this.channelArr.map(item => {
         return item.id
       })
       //判断getAllChannelList是否包含channelArr里的id
-      let otherChannelList= this.allChannelList.filter(item=>{
-         return !ids.includes(item.id)
+      let otherChannelList = this.allChannelList.filter(item => {
+        return !ids.includes(item.id)
       })
       //返回推荐列表
       return otherChannelList
